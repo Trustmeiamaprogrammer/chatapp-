4e44d5eda360b834b6350c475be2c728ac8b6bd6
Trustmeiamaprogrammer <36696989+Trustmeiamaprogrammer@users.noreply.github.com>
2019-10-07 15:35:07 +0200

    GebruikersActivity aangepast

--- app/src/main/java/com/example/chatapp/GebruikersActivity.java
+++ app/src/main/java/com/example/chatapp/GebruikersActivity.java
@@ -1,14 +1,43 @@
 package com.example.chatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class GebruikersActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private RecyclerView mGebruikerslijst;
+    private DatabaseReference gebruikersRef;
+    private LinearLayoutManager mLayoutManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gebruikers);
+
+        mToolbar = (Toolbar) findViewById(R.id.gebruikersAppBar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Gebruikers");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        gebruikersRef = FirebaseDatabase.getInstance().getReference().child("Gebruikers");
+        mLayoutManager = new LinearLayoutManager(this);
+        mGebruikerslijst = (RecyclerView) findViewById(R.id.gebruikersLijst);
+        mGebruikerslijst.setHasFixedSize(true);
+        mGebruikerslijst.setLayoutManager(mLayoutManager);
+
+    }
+
+    protected void onStart()
+    {
+        super.onStart();
+
     }
 }
--- app/src/main/java/com/example/chatapp/VriendenFrag.java
+++ app/src/main/java/com/example/chatapp/VriendenFrag.java
@@ -4,6 +4,7 @@ package com.example.chatapp;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
@@ -12,6 +13,7 @@ import android.view.ViewGroup;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 /**
@@ -39,7 +41,35 @@ public class VriendenFrag extends Fragment {
 
         mMainView = inflater.inflate(R.layout.fragment_vrienden, container, false);
         mVriendenLijst = (RecyclerView) mMainView.findViewById(R.id.vriendenLijst);
+        mAuth = FirebaseAuth.getInstance();
+        huidigeGebruikerId = mAuth.getCurrentUser().getUid();
+        vriendenDatabase = FirebaseDatabase.getInstance().getReference().child("Vrienden").child(huidigeGebruikerId);
+        vriendenDatabase.keepSynced(true);
+        gebruikersRef = FirebaseDatabase.getInstance().getReference().child("Gebruikers");
+        gebruikersRef.keepSynced(true);
+
+        mVriendenLijst.setHasFixedSize(true);
+        mVriendenLijst.setLayoutManager(new LinearLayoutManager(getContext()));
         return mMainView;
+    }
+
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+        FirebaseRecyclerAdapter<Vrienden, VriendenViewHolder> vriendenRecyclerViewAdapter = new FirebaseRecyclerAdapter<Vrienden, vriendenViewHolder>
+        {
+            Vrienden.class,
+            R.layout.gebruikers_layout,
+            VriendenViewHolder.class,
+            vriendenDatabase
+        } {
+        @Override
+       protected void toonViewHolder (final VriendenViewHolder vriendenViewHolder, Vrienden vrienden, int i)
+        {
+            vriendenViewHolder.setDate(vrienden.getDate());
+            final String
+        }
     }
 
 }
