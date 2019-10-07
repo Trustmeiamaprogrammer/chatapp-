e3cda8c4f8319df620d983426f5250fbd63ccf0c
Trustmeiamaprogrammer <36696989+Trustmeiamaprogrammer@users.noreply.github.com>
2019-10-07 13:47:29 +0200

    Instellingen en Gebruikersactivity gemaakt.

--- app/src/main/java/com/example/chatapp/MainActivity.java
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.chatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
@@ -13,6 +12,10 @@ import android.view.MenuItem;
 import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,8 +24,7 @@ public class MainActivity extends AppCompatActivity {
     private ViewPager mViewpager;
     private SectiePagerAdapter mSectiePagerAdapter;
     private TabLayout mTabLayout;
-
-
+    private DatabaseReference mGebruikerRef;
 
 
     @Override
@@ -36,29 +38,48 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("Chat app");
 
+        if (mAuth.getCurrentUser() != null)
+        {
+            mGebruikerRef = FirebaseDatabase.getInstance().getReference().child("Gebruikers").child(mAuth.getCurrentUser().getUid());
+        }
+
+        // Tabbladen
         mViewpager = (ViewPager) findViewById(R.id.main_view);
         mSectiePagerAdapter = new SectiePagerAdapter(getSupportFragmentManager());
         mViewpager.setAdapter(mSectiePagerAdapter);
 
         mTabLayout = (TabLayout) findViewById(R.id.tabs);
         mTabLayout.setupWithViewPager(mViewpager);
-
-
-
-
-
     }
 
     @Override
     public void onStart() {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
+        FirebaseUser huidigeGebruiker = mAuth.getCurrentUser();
 
-        if (currentUser == null)
+        if (huidigeGebruiker == null)
         {
            sendToStart();
         }
+
+        else
+        {
+            mGebruikerRef.child("online").setValue("true");
+        }
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        FirebaseUser huidigeGebruiker = mAuth.getCurrentUser();
+
+        if(huidigeGebruiker != null)
+        {
+            mGebruikerRef.child("online").setValue(ServerValue.TIMESTAMP);
+        }
+
     }
 
     private void sendToStart() {
@@ -82,9 +103,23 @@ public class MainActivity extends AppCompatActivity {
 
         if(item.getItemId() == R.id.mainLoguitKnop)
         {
+            mGebruikerRef.child("online").setValue(ServerValue.TIMESTAMP);
             FirebaseAuth.getInstance().signOut();
             sendToStart();
         }
+
+        if(item.getItemId() == R.id.mainAinstKnop)
+        {
+            Intent instellingenIntent = new Intent(MainActivity.this, InstellingenActivity.class);
+            startActivity(instellingenIntent);
+        }
+
+        if(item.getItemId() == R.id.mainGebruikersKnop)
+        {
+            Intent gebruikersIntent = new Intent(MainActivity.this, GebruikersActivity.class);
+            startActivity(gebruikersIntent);
+        }
+
 
 
         return true;
